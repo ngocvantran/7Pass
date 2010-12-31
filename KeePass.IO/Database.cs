4f8f4e5d6621ce2e262b89cc102ea69110ef7f19
Tran Ngoc Van <wp7pass@hotmail.com>
2011-01-01 02:02:46 +0800

    Added parsing of custom icons.

--- KeePass.IO/Database.cs
+++ KeePass.IO/Database.cs
@@ -1,42 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Windows.Media;
 
 namespace KeePass.IO
 {
     public class Database
     {
+        private readonly IDictionary<Guid, ImageSource> _customIcons;
         private readonly IDictionary<Guid, Entry> _entries;
         private readonly IDictionary<Guid, Group> _groups;
 
         private readonly Group _root;
 
+        /// <summary>
+        /// Gets all entries.
+        /// </summary>
         public IEnumerable<Entry> Entries
         {
             get { return _entries.Values; }
         }
 
+        /// <summary>
+        /// Gets the flattened list of groups.
+        /// </summary>
         public IEnumerable<Group> Groups
         {
             get { return _groups.Values; }
         }
 
+        /// <summary>
+        /// Gets the custom icons.
+        /// </summary>
+        public IDictionary<Guid, ImageSource> Icons
+        {
+            get { return _customIcons; }
+        }
+
+        /// <summary>
+        /// Gets the root group.
+        /// </summary>
         public Group Root
         {
             get { return _root; }
         }
 
-        public Database(Group root)
+        public Database(Group root,
+            IDictionary<Guid, ImageSource> customIcons)
         {
-            if (root == null)
-                throw new ArgumentNullException("root");
+            if (root == null) throw new ArgumentNullException("root");
+            if (customIcons == null) throw new ArgumentNullException("customIcons");
 
             _root = root;
+            _customIcons = customIcons;
             _groups = new Dictionary<Guid, Group>();
             _entries = new Dictionary<Guid, Entry>();
 
             Index(root);
         }
 
+        /// <summary>
+        /// Gets the specified entry.
+        /// </summary>
+        /// <param name="id">The entry id.</param>
+        /// <returns>Entry with the specified id, or <c>null</c> if not found.</returns>
         public Entry GetEntry(Guid id)
         {
             Entry entry;
@@ -44,6 +70,11 @@ namespace KeePass.IO
                 ? entry : null;
         }
 
+        /// <summary>
+        /// Gets the specified group.
+        /// </summary>
+        /// <param name="id">The group id.</param>
+        /// <returns>Group with the specified id, or <c>null</c> if not found.</returns>
         public Group GetGroup(Guid id)
         {
             Group group;
