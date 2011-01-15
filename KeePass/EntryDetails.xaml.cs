cbf1f0dc8532370fc278db4373248b638548b43d
Tran Ngoc Van <wp7pass@hotmail.com>
2011-01-15 16:58:54 +0800

    Added Entry page.

---
+++ KeePass/EntryDetails.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Navigation;
+using KeePass.Storage;
+
+namespace KeePass
+{
+    public partial class EntryDetails
+    {
+        private bool _loaded;
+
+        public EntryDetails()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(
+            bool cancelled, NavigationEventArgs e)
+        {
+            if (cancelled || _loaded)
+                return;
+
+            var database = Cache.Database;
+            if (database == null)
+            {
+                GoBack<MainPage>();
+                return;
+            }
+
+            _loaded = true;
+            var id = NavigationContext.QueryString["id"];
+            DataContext = database.GetEntry(id);
+        }
+
+        private void cmdHome_Click(object sender, EventArgs e)
+        {
+            GoBack<GroupDetails>();
+        }
+    }
+}
