723171230b713159f8aebdc6437b29805a9c9b66
Tran Ngoc Van <wp7pass@hotmail.com>
2011-01-17 01:01:29 +0800

    Added InputScope and Enter key handling. Added Box.net template.

--- KeePass/Sources/Web/WebAuth.xaml.cs
+++ KeePass/Sources/Web/WebAuth.xaml.cs
@@ -1,11 +1,21 @@
 ﻿using System;
 using System.Net;
 using System.Windows;
+using System.Windows.Input;
+using KeePass.Utils;
 
 namespace KeePass.Sources.Web
 {
     public partial class WebAuth
     {
+        public event EventHandler Completed;
+
+        public bool UseAuth
+        {
+            get { return chkAuth.IsChecked == true; }
+            set { chkAuth.IsChecked = value; }
+        }
+
         public WebAuth()
         {
             InitializeComponent();
@@ -13,7 +23,7 @@ namespace KeePass.Sources.Web
 
         public NetworkCredential GetCredentials()
         {
-            if (!chkAuth.IsChecked == true)
+            if (!UseAuth)
                 return null;
 
             return WebUtils.CreateCredentials(
@@ -21,9 +31,20 @@ namespace KeePass.Sources.Web
                 txtDomain.Text);
         }
 
-        private void chkAuth_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Raises the <see cref="Completed"/> event.
+        /// </summary>
+        /// <param name="e">The <see cref="System.EventArgs"/>
+        /// instance containing the event data.</param>
+        protected virtual void OnCompleted(EventArgs e)
         {
-            var visible = chkAuth.IsChecked == true
+            if (Completed != null)
+                Completed(this, e);
+        }
+
+        private void chkAuth_CheckedChanged(object sender, RoutedEventArgs e)
+        {
+            var visible = UseAuth
                 ? Visibility.Visible
                 : Visibility.Collapsed;
 
@@ -33,6 +54,24 @@ namespace KeePass.Sources.Web
             txtDomain.Visibility = visible;
             lblPassword.Visibility = visible;
             txtPassword.Visibility = visible;
+        }
+
+        private void txtDomain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.IsEnter())
+                OnCompleted(EventArgs.Empty);
+        }
+
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.IsEnter())
+                txtDomain.Focus();
+        }
+
+        private void txtUser_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.IsEnter())
+                txtPassword.Focus();
         }
     }
 }
