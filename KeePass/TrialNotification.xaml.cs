d1bd78d4ceffb4a1f0f9e4e3e552e317cc3a6309
Tran Ngoc Van <wp7pass@hotmail.com>
2011-01-16 17:44:06 +0800

    Added trial notification.

---
+++ KeePass/TrialNotification.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
+using KeePass.Utils;
+using Microsoft.Phone.Tasks;
+
+namespace KeePass
+{
+    public partial class TrialNotification
+    {
+        private readonly DispatcherTimer _tmrHide;
+        private readonly DispatcherTimer _tmrShow;
+
+        public TrialNotification()
+        {
+            InitializeComponent();
+
+            _tmrShow = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(3),
+            };
+            _tmrShow.Tick += _tmrShow_Tick;
+
+            _tmrHide = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(10),
+            };
+            _tmrHide.Tick += _tmrHide_Tick;
+        }
+
+        private void _tmrHide_Tick(object sender, EventArgs e)
+        {
+            _tmrHide.Stop();
+
+            var story = (Storyboard)Resources["hideBoard"];
+            story.Begin();
+        }
+
+        private void _tmrShow_Tick(object sender, EventArgs e)
+        {
+            _tmrShow.Stop();
+            popupTrial.IsOpen = true;
+            TrialManager.PopupShown();
+
+            _tmrHide.Start();
+        }
+
+        private void lnkBuy_Click(object sender, RoutedEventArgs e)
+        {
+            new MarketplaceDetailTask().Show();
+        }
+
+        private void popupTrial_Loaded(object sender, RoutedEventArgs e)
+        {
+            _tmrShow.Start();
+        }
+
+        private void story_Completed(object sender, EventArgs e)
+        {
+            popupTrial.IsOpen = false;
+        }
+    }
+}
