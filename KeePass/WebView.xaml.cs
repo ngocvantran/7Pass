9087e2a3033d284c9eaeb58793d03b3caaef0133
Tran Ngoc Van <wp7pass@hotmail.com>
2011-02-11 20:47:35 +0800

    Added integrated web browser.

---
+++ KeePass/WebView.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using KeePass.IO;
+using KeePass.Storage;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace KeePass
+{
+    public partial class WebView
+    {
+        private Entry _entry;
+
+        public WebView()
+        {
+            InitializeComponent();
+        }
+
+        private void browser_Loaded(object sender, RoutedEventArgs e)
+        {
+            var url = new Uri(
+                NavigationContext.QueryString["url"],
+                UriKind.Absolute);
+
+            browser.Navigate(url);
+        }
+
+        protected override void OnNavigatedTo(
+            bool cancelled, NavigationEventArgs e)
+        {
+            if (cancelled || _entry != null)
+                return;
+
+            var database = Cache.Database;
+            if (database == null)
+            {
+                GoBack<MainPage>();
+                return;
+            }
+
+            var id = NavigationContext.QueryString["entry"];
+            _entry = database.GetEntry(id);
+
+            foreach (var field in _entry.CustomFields.Take(3))
+            {
+                var item = new ApplicationBarMenuItem(field.Key);
+                item.Click += (s, _) => SetValue(field.Value);
+                ApplicationBar.MenuItems.Add(item);
+            }
+        }
+
+        private void cmdPassword_Click(object sender, EventArgs e)
+        {
+            SetValue(_entry.Password);
+        }
+
+        private void SetValue(string value)
+        {
+            try
+            {
+                browser.Focus();
+
+                value = value.Replace(@"\", @"\\");
+                var script = string.Format(
+                    "document.activeElement.value='{0}'", value);
+                browser.InvokeScript("eval", script);
+            }
+            catch (Exception)
+            {
+                lblOverlay.Text = value;
+                vwOverlay.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void cmdUser_Click(object sender, EventArgs e)
+        {
+            SetValue(_entry.UserName);
+        }
+
+        private void cmdRefresh_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                browser.InvokeScript("eval", "history.go()");
+            }
+            catch
+            {
+                browser.Navigate(browser.Source);
+            }
+        }
+
+        private void cmdBack_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                browser.InvokeScript("eval", "history.go(-1)");
+            }
+            catch
+            {
+            }
+        }
+
+        private void vwOverlay_Flick(object sender, FlickGestureEventArgs e)
+        {
+            if (e.Direction != System.Windows.Controls.Orientation.Vertical)
+                return;
+
+            if (e.VerticalVelocity >= 0)
+                return;
+
+            e.Handled = true;
+            vwOverlay.Visibility = Visibility.Collapsed;
+        }
+
+        private void browser_Navigated(object sender, NavigationEventArgs e)
+        {
+            progBusy.IsLoading = true;
+            progBusy.Visibility = Visibility.Visible;
+        }
+
+        private void browser_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            progBusy.IsLoading = false;
+            progBusy.Visibility = Visibility.Collapsed;
+        }
+    }
+}
