100cb4b8ae23d67743f123581982acf35ee6ed51
Tran Ngoc Van <wp7pass@hotmail.com>
2011-09-30 23:17:24 +0800

    Fixed Submission error 1047. Fixed MarketPlace image size.

--- KeePass.IO/Properties/AssemblyInfo.cs
+++ KeePass.IO/Properties/AssemblyInfo.cs
@@ -1,36 +1,11 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KeePass.IO")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tran Ngoc Van")]
-[assembly: AssemblyProduct("KeePass.IO")]
-[assembly: AssemblyCopyright("Copyright © Tran Ngoc Van 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("3e288e1c-23c4-4db9-852e-68cc7aa7a167")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("3.2.0.0")]
-[assembly: AssemblyFileVersion("3.2.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
