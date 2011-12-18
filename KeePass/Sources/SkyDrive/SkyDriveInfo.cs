e024b67b148ba7ebb332f720c0e1c8b829e72983
Tran Ngoc Van <wp7pass@hotmail.com>
2011-12-18 14:46:59 +0800

    Removed developer specific information.

--- KeePass/Sources/SkyDrive/SkyDriveInfo.cs
+++ KeePass/Sources/SkyDrive/SkyDriveInfo.cs
@@ -4,7 +4,8 @@ namespace KeePass.Sources.SkyDrive
 {
     internal static class SkyDriveInfo
     {
-        public const string CLIENT_ID = "0000000040070479";
-        public const string REDIRECT = "http://7pass.wordpress.com/";
+#warning You need your SkyDrive information
+        public const string CLIENT_ID = "YOUR_CLIENT_ID";
+        public const string REDIRECT = "YOUR_REDIRECT";
     }
 }
