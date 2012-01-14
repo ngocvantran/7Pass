e42f73e73538fddcc7292df3fcf03ec81aba4e76
Tran Ngoc Van <wp7pass@hotmail.com>
2012-01-14 17:13:13 +0800

    Minor API keys tweak.

--- KeePass/Utils/ApiKeys.cs
+++ KeePass/Utils/ApiKeys.cs
@@ -4,11 +4,11 @@ namespace KeePass.Utils
 {
     internal static class ApiKeys
     {
-#warning 7Pass needs API Keys to use cloud services
-        public const string DROPBOX_KEY = "DROPBOX_KEY";
-        public const string DROPBOX_SECRET = "DROPBOX_SECRET";
-        public const string MIXPANEL_TOKEN = "MIXPANEL_TOKEN";
-        public const string SKYDRIVE_CLIENT_ID = "SKYDRIVE_CLIENT_ID";
-        public const string SKYDRIVE_REDIRECT = "SKYDRIVE_REDIRECT";
+#warning 7Pass needs API Keys to use web services
+        public const string DROPBOX_KEY = "YOUR_DROPBOX_KEY";
+        public const string DROPBOX_SECRET = "YOUR_DROPBOX_SECRET";
+        public const string MIXPANEL_TOKEN = "YOUR_MIXPANEL_TOKEN";
+        public const string SKYDRIVE_CLIENT_ID = "YOUR_SKYDRIVE_CLIENT_ID";
+        public const string SKYDRIVE_REDIRECT = "YOUR_SKYDRIVE_REDIRECT";
     }
 }
